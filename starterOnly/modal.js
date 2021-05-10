eb5e0755148df38e55738a11df07b19f21fab592
elodie baeza <elodie.baeza@gmail.com>
2021-05-11 00:02:53 +0200

    fix from 2nd pull request remarks

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,19 +1,19 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += 'responsive';
   } else {
-    x.className = "topnav";
+    x.className = 'topnav';
   }
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const closeBtn = document.querySelectorAll(".close");
+const modalbg = document.querySelector('.bground');
+const modalBtn = document.querySelectorAll('.modal-btn');
+const closeBtn = document.querySelectorAll('.close');
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
 
 // launch modal form
 function launchModal() {
@@ -21,43 +21,43 @@ function launchModal() {
 }
 
 // close modal event
-closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+closeBtn.forEach((btn) => btn.addEventListener('click', closeModal));
  
 // close modal form
 function closeModal() {
-  modalbg.style.display = "none";
+  modalbg.style.display = 'none';
 }
 
 // #2 TO DO: implement form entries
  
  // DOM elements
 //  const form = document.forms["reserve"];
-const form = document.getElementById("reserve");
-const firstName = document.getElementById("first");
-const lastName = document.getElementById("last");
-const birthDate = document.getElementById("birthdate");
-const checkbox = document.querySelectorAll('input[type="checkbox"]');
+const form = document.getElementById('reserve');
+const firstName = document.getElementById('first');
+const lastName = document.getElementById('last');
+const birthDate = document.getElementById('birthdate');
+const checkbox1 = document.getElementById('checkbox1');
 const inputs = form.getElementsByTagName('input');
 const formData = document.getElementsByClassName('formData');
 
 // valide si prenom n'est pas vide et au mini 2 caractères
-function validateText(t){
-	return (t !== '' && t.length >= 2);
+function validateText(text) {
+	return (text.value !== '' && text.value.length >= 2);
 }
 
 // valide syntaxe dans champ email
-function validateEmail(e){
-	return (/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(e));
+function validateEmail() {
+	return (/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(inputs['email'].value));
 }
 
 // valide syntaxe dans champ date de naissance
-function validateDate(d){
-	return (/^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/.test(d));
+function validateDate() {
+	return (/^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/.test(inputs['birthdate'].value));
 } 
 
 // valide champ nbr de tournois non vide
-function validateNbr(n){
-	return n !== '';
+function validateNbr(nbrTournois) {
+	return nbrTournois.value !== '';
 } 
 
 // valide si ville cochée
@@ -66,22 +66,22 @@ function validateRadio() {
 }
 
 // valide si conditions d'utilisations cochée
-function validateCheckbox(i){
-	return (checkbox[i].checked);
+function validateCheckbox() {
+	return (checkbox1.checked);
 }
 
 // vérification des contraintes lors de la validation du formulaire
-function validate(){
+function validateForm() {
 
 	//cré un tableau des contraintes valides et non valides	
 	const inputsItems = [
-		validateText(inputs["first"].value),
-		validateText(inputs["last"].value),
-		validateEmail(inputs["email"].value),
-		validateDate(inputs["birthdate"].value),
-		validateNbr(inputs["quantity"].value),
+		validateText(inputs['first']),
+		validateText(inputs['last']),
+		validateEmail(),
+		validateDate(),
+		validateNbr(inputs['quantity']),
 		validateRadio(),
-		validateCheckbox(0)
+		validateCheckbox()
 	];
 
 	//si le tableau des contraintes contient false alors retourne !true => false
